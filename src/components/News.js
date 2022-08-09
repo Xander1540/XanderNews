a11027b4f63679e435c9ce5eb30eb74d5dccd52f
ADITYA NARAYAN VERMA <86612839+Xander1540@users.noreply.github.com>
2022-08-09 22:38:29 +0530

    First commit

---
+++ src/components/News.js
@@ -0,0 +1,16 @@
+//rce
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a News component.
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let {title, description}= this.props;
+    return (
+      <div>
+        <div className="card" style="width: 18rem">
+          <img src="..." className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title"> {title} </h5>
+            <p className="card-text"> {description} </p>
+            <a href="/" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
